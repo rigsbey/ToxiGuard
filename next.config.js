fa0502a64f80c05884c5efd119d1e5543fc4c701
kamil <k.abulkhanov@yallamart.ae>
2025-02-07 12:54:26 +0100

    fix: add sitemap and domain configs

--- next.config.js
+++ next.config.js
@@ -16,6 +16,15 @@ module.exports = {
   async headers() {
     return [
       {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml',
+          },
+        ],
+      },
+      {
         source: '/(.*)',
         headers: [
           {
