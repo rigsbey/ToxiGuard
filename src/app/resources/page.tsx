25d3667f39ba7913bde76b6aceb0a9f571989d1e
kamil <k.abulkhanov@yallamart.ae>
2025-02-12 20:57:10 +0100

    deleted some resources for google search removed

--- src/app/resources/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function ResourcesPage() {
-  // Так как раздел экспертных ресурсов удалён, перенаправляем посетителей в блог
-  redirect('/blog');
-  return null;
-} 
