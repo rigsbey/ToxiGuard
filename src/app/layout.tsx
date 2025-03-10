1a290460737ec4db79aae402aac721e9642792ea
kamil <abulhanov@vbankcenter.ru>
2025-03-10 21:37:58 +0100

    feat: fix for icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from 'next';
+import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'ToxiGuard',
+  title: '🛡️ToxiGuard',
   description: 'AI-Powered Risk Scanner for Freelancers',
 };
 
@@ -21,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🛡️</text></svg>" />
+      </head>
       <body className={inter.className}>
         <main>{children}</main>
       </body>
